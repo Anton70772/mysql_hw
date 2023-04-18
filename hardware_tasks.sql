e225d60a9f5b62556911843f76452c52a7200b10
Anton70772 <116624941+Anton70772@users.noreply.github.com>
2023-04-18 18:30:04 +0300

    hsql5

--- hardware_tasks.sql
+++ hardware_tasks.sql
@@ -1,1 +1,30 @@
-SELECT AVG(price) FROM hardware;
+-- 1.
+  SELECT count(*) from hardware;
+-- 2. 
+  SELECT * FROM hardware WHERE amount = 0;
+-- 3.
+  SELECT AVG(price) FROM hardware;
+-- 4.
+SELECT * FROM hardware WHERE title LIKE '%Клавиатура%'
+order by price asc
+;
+-- 5.
+select title, amount, tag from hardware
+order by amount desc
+;
+-- 6.
+select title, tag from hardware
+where tag like '%discount%'
+;
+-- 7.
+SELECT hardware.title, hardware.price
+FROM hardware
+WHERE hardware.tag = 'new'
+ORDER BY hardware.price DESC
+LIMIT 1;
+-- 8.
+INSERT INTO `hardware` (`title`, `price`, `amount`, `tag`) 
+VALUES
+  ('Ноутбук Lenovo 2BXKQ7E9XD', '54500', '1', 'new');
+  -- 9.
+  DELETE FROM hardware WHERE id IN (18);
